044c81d4d8fd1b9ddfa74ed8e854cb35c0c69174
Grem7 <60433364+Grem7@users.noreply.github.com>
2020-11-10 22:46:49 -0500

    Révision du programme selon les normes PEP8

--- exercice.py
+++ exercice.py
@@ -4,22 +4,22 @@
 
 # TODO: Importez vos modules ici
 import numpy as np
+import math
 
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+    return np.linspace(start=-1.3, stop=3.5, num=64)
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+    return [(math.sqrt(coord[0]**2 + coord[1]**2), math.atan2(coord[1]. coord[0])) for coord in cartesian_coordinates]
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
-    return 0
+    return np.argmin(np.absolute(values - float))
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
     pass
